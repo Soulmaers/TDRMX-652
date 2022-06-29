f5ea900cfb37a41c7bbed21b67130a081bc1abe0
Soulmaers <certiss@ya.ru>
2022-06-29 18:49:01 +0300

    !

--- public/js/script.js
+++ public/js/script.js
@@ -54,6 +54,7 @@ function getMainInfo() {
             arrayT.push(arr[11]); arrayT.push(arr[19]); arrayT.push(arr[28]); arrayT.push(arr[27]);
 
             funcRandom(arrayD, arrayT);
+            tiresOs(arrayD);
             // go(arrayD, arrayT);
             //return window['arrayD'] = arrayD, arrayT, arr
         });
@@ -91,8 +92,91 @@ function getMainInfo() {
     */
 }
 
+//подсветка оси
+/*
+function tiresOs(arr) {
+    oneLow = document.querySelectorAll('.oneLow');
+    twoLow = document.querySelectorAll('.twoLow');
+    threeLow = document.querySelectorAll('.threeLow');
+    arrFront = arr.slice(0, 2);
+    arrCenter = arr.slice(2, 6);
+    arrRear1 = arr.slice(6, 8);
+    arrRear2 = arr.slice(8, 10);
+    arrRear3 = arr.slice(10, 12);
+    oneLow.forEach(function (elem, index) {
+        elem.style.background = objColor[generFront(arrFront[index])];
+    })
+    twoLow.forEach(function (elem, index) {
+        elem.style.background = objColor[generFront(arrCenter[index])];
+    })
+    threeLow.forEach(function (elem, index) {
+        elem.style.background = objColor[generRear(arrRear1[index])];
+    })
 
-//проверяем условия
+}*/
+
+function tiresOs(arr) {
+    arrFront = arr.slice(0, 2);
+    arrCenter = arr.slice(2, 6);
+    arrRear1 = arr.slice(6, 8);
+    arrRear2 = arr.slice(8, 10);
+    arrRear3 = arr.slice(10, 12);
+    tiresTest(arrFront, arrCenter, arrRear1, arrRear2, arrRear3)
+}
+function tiresTest(arr1, arr2, arr3, arr4, arr5) {
+    lowDiv = document.querySelectorAll('.lowDiv ');
+
+    if (arr1[0] >= 8 && arr1[0] <= 9 || arr1[1] >= 8 && arr1[1] <= 9) {
+        lowDiv[0].style.backgroundColor = '#3eb051'
+    }
+    if (arr1[0] >= 7.5 && arr1[0] < 8 || arr1[0] > 9 && arr1[0] <= 13 || arr1[1] >= 7.5 && arr1[1] < 8 || arr1[1] > 9 && arr1[1] <= 13) {
+        lowDiv[0].style.backgroundColor = '#9ba805'
+    }
+    if (arr1[0] > -100 && arr1[0] < 7.5 || arr1[0] > 13 || arr1[1] > -100 && arr1[1] < 7.5 || arr1[1] > 13) {
+        lowDiv[0].style.backgroundColor = '#e03636'
+    }
+    if (arr3[0] >= 9 && arr3[0] <= 12 || arr3[1] >= 9 && arr3[1] <= 12) {
+        lowDiv[2].style.backgroundColor = '#3eb051'
+    }
+    if (arr3[0] >= 8 && arr3[0] < 9 || arr3[0] > 12 && arr3[0] <= 13 || arr3[1] >= 8 && arr3[1] < 9 || arr3[1] > 12 && arr3[1] <= 13) {
+        lowDiv[2].style.backgroundColor = '#9ba805'
+    }
+    if (arr3[0] > -100 && arr3[0] < 8 || arr3[0] > 13 || arr3[1] > -100 && arr3[1] < 8 || arr3[1] > 13) {
+        lowDiv[2].style.backgroundColor = '#e03636'
+    }
+    if (arr4[0] >= 9 && arr4[0] <= 12 || arr4[1] >= 9 && arr4[1] <= 12) {
+        lowDiv[3].style.backgroundColor = '#3eb051'
+    }
+    if (arr4[0] >= 8 && arr4[0] < 9 || arr4[0] > 12 && arr4[0] <= 13 || arr4[1] >= 8 && arr4[1] < 9 || arr4[1] > 12 && arr4[1] <= 13) {
+        lowDiv[3].style.backgroundColor = '#9ba805'
+    }
+    if (arr4[0] > -100 && arr4[0] < 8 || arr4[0] > 13 || arr4[1] > -100 && arr4[1] < 8 || arr4[1] > 13) {
+        lowDiv[3].style.backgroundColor = '#e03636'
+    }
+    if (arr5[0] >= 9 && arr5[0] <= 12 || arr5[1] >= 9 && arr5[1] <= 12) {
+        lowDiv[4].style.backgroundColor = '#3eb051'
+    }
+    if (arr5[0] >= 8 && arr5[0] < 9 || arr5[0] > 12 && arr5[0] <= 13 || arr5[1] >= 8 && arr5[1] < 9 || arr5[1] > 12 && arr5[1] <= 13) {
+        lowDiv[4].style.backgroundColor = '#9ba805'
+    }
+    if (arr5[0] > -100 && arr5[0] < 8 || arr5[0] > 13 || arr5[1] > -100 && arr5[1] < 8 || arr5[1] > 13) {
+        lowDiv[4].style.backgroundColor = '#e03636'
+    }
+    if (arr2[0] >= 8 && arr2[0] <= 9 || arr2[1] >= 8 && arr2[1] <= 9 || arr2[2] >= 8 && arr2[2] <= 9
+        || arr2[2] >= 8 && arr2[2] <= 9 || arr2[3] >= 8 && arr2[3] <= 9 || arr2[3] >= 8 && arr2[3] <= 9) {
+        lowDiv[1].style.backgroundColor = '#3eb051'
+    }
+    if (arr2[0] >= 7.5 && arr2[0] < 8 || arr2[0] > 9 && arr2[0] <= 13 || arr2[1] >= 7.5 && arr2[1] < 8 || arr2[1] > 9 && arr2[1] <= 13
+        || arr2[2] >= 7.5 && arr2[2] < 8 || arr2[2] > 9 && arr2[2] <= 13 || arr2[3] >= 7.5 && arr2[3] < 8 || arr2[3] > 9 && arr2[3] <= 13) {
+        lowDiv[1].style.backgroundColor = '#9ba805'
+    }
+    if (arr2[0] > -100 && arr2[0] < 7.5 || arr2[0] > 13 || arr2[1] > -100 && arr2[1] < 7.5 || arr2[1] > 13
+        || arr2[2] > -100 && arr2[2] < 7.5 || arr2[2] > 13 || arr2[3] > -100 && arr2[3] < 7.5 || arr2[3] > 13) {
+        lowDiv[1].style.backgroundColor = '#e03636'
+    }
+
+}
+
 function generFront(el) {
     let generatedValue;
     if (el >= 8 && el <= 9)
@@ -152,7 +236,7 @@ const logic733 = (el1, el2) => {
             // console.log(localStorage.getItem('name'));
         }
         else {
-            elem.textContent = parseFloat(elem[index]) + '\nБар';
+            elem.textContent = parseFloat(arrTiresFront[index]) + '\nБар';
             // localStorage.setItem('name', elem.textContent);
             // localStorage.getItem('name');
             //console.log(localStorage.getItem('name'));
@@ -164,7 +248,7 @@ const logic733 = (el1, el2) => {
             elem.textContent = '-';
         }
         else {
-            elem.textContent = parseFloat(elem[index]) + '\nБар';
+            elem.textContent = parseFloat(arrTiresRear[index]) + '\nБар';
         }
     })
     allsT.forEach(function (elem, index) {
@@ -247,7 +331,7 @@ const tires_link = document.querySelectorAll('.tires_link');
 const arrTireslink = Array.from(tires_link);
 
 //проваливаемся в колесо
-/*
+
 arrTireslink.forEach(function (elem, index) {
     elem.addEventListener('click', tiresActive);
     function tiresActive() {
@@ -261,35 +345,21 @@ arrTireslink.forEach(function (elem, index) {
         tT = document.querySelectorAll('.tiresT');
         elem = tD[index].classList.toggle('tiresActiveD');
         elem = tT[index].classList.toggle('tiresActiveT');
-        check = document.querySelector('.check')
+        //check = document.querySelector('.check')
         //check.style.display = 'none';
-        dataActive = document.querySelector('.dataActive')
+        /*dataActive = document.querySelector('.dataActive')
         dataActive.style.display = 'block';
         wCA = document.querySelector('.wrapper_containt')
-        wCA.classList.add('wrapper_containt_active')
+        wCA.classList.add('wrapper_containt_active')*/
         grafik = document.querySelector('.grafik');
         grafik.style.display = 'block';
     }
- 
+
 });
- 
+
 arrTireslink.forEach((elem, index) => {
     elem.addEventListener('click', tiresLinkfunc);
     function tiresLinkfunc() {
-        const dataMade = document.querySelector('.data_made');
-        const probeg = document.querySelector('.probeg');
-        const runShine = Array(10).fill(0).map(runTires);
-        function tiresActivedatchik() {
-            elem = runShine[index];
-            probeg.textContent = elem + ' ' + 'км';
-        }
-        tiresActivedatchik();
-        function dataMadefunc() {
-            elem = arData[index];
-            dataMade.textContent = elem;
-        }
-        dataMadefunc();
- 
         function tiresGrafik() {
             elem = arrAll1[index];
             davl = elem;
@@ -303,7 +373,7 @@ arrTireslink.forEach((elem, index) => {
         chrt();
     }
 });
- 
+
 function chrt() {
     Chart.register(ChartDataLabels);
     myChartg = new Chart(myChartg, {
@@ -368,14 +438,14 @@ function chrt() {
             },
         }
     });
- 
+
     const upDia = () => {
         myChartg.data.datasets[0].data = davl;
         myChartg.data.datasets[1].data = davl2;
         myChartg.update();
     }
     setInterval(upDia, 100);
-}*/
+}
 const arrAll1 = [[], [], [], [], [], [], [], [], [], []];
 const arrAll2 = [[], [], [], [], [], [], [], [], [], []];
 arrTime = [];
@@ -396,7 +466,7 @@ function getNowtime1() {
     let nowTime = (`${hours}:${minutes}:${seconds}`);
     return nowTime;
 }
-/*
+
 function go(item1, item2) {
     arrTime.push(getNowtime1());
     item1.forEach((el, index) => {
@@ -406,7 +476,7 @@ function go(item1, item2) {
         arrAll2[index].push(parseFloat(el.toFixed(0)));
     })
     return arrAll1, arrAll2
-}*/
+}
 
 
 
@@ -441,24 +511,36 @@ function getSensorInfo() { // get and show information about selected Sensor
 
 
 function dashDav() {
-    const arrDall = arrayD;
+    const arrTiresFront = arrayD.slice(0, 6);
+    const arrTiresRear = arrayD.slice(6, 12);
     countRed = 0;
     countYellow = 0;
     countGreen = 0;
-    arrDall.forEach((el) => {
-        if (el >= 8 && el <= 10) {
+    arrTiresFront.forEach((el) => {
+        if (el >= 8 && el <= 9) {
             countGreen++
         }
-        if (el >= 7 && el < 8 || el > 10 && el <= 11) {
+        if (el >= 7.5 && el < 8 || el > 9 && el <= 13) {
             countYellow++
         }
-        if (el >= -1000 && el < 7 || el > 11 || el === -348201.3876) {
+        if (el > -100 && el < 7.5 || el > 13 || el === -348201.3876) {
             countRed++
         }
     })
-    resultRed = Math.round(countRed / arrDall.length * 100);
-    resultYellow = Math.round(countYellow / arrDall.length * 100);
-    resultGreen = Math.round(countGreen / arrDall.length * 100);
+    arrTiresRear.forEach((el) => {
+        if (el >= 9 && el <= 12) {
+            countGreen++
+        }
+        if (el > 8 && el < 9 || el > 12 && el <= 13) {
+            countYellow++
+        }
+        if (el > -100 && el < 8 || el > 13 || el === -348201.3876) {
+            countRed++
+        }
+    })
+    resultRed = Math.round(countRed / arrayD.length * 100);
+    resultYellow = Math.round(countYellow / arrayD.length * 100);
+    resultGreen = Math.round(countGreen / arrayD.length * 100);
     return arrD = [resultRed, resultYellow, resultGreen];
 }
 
@@ -486,6 +568,13 @@ chart = new Chart(ctx, {
     },
     options: {
         plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 10
+                    }
+                }
+            },
             datalabels: {
                 color: '#423737',
                 textAlign: 'center',
@@ -545,6 +634,13 @@ const chart2 = new Chart(ctx2, {
     },
     options: {
         plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 10
+                    }
+                }
+            },
             datalabels: {
                 color: '#423737',
                 textAlign: 'center',
