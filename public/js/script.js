01eb41c402d4c95d25a515ab2892c936a3144388
Soulmaers <certiss@ya.ru>
2022-09-02 08:27:27 +0300

    !

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,8 @@
 
 
 
-/*
-const detaly = document.querySelector('.detaly');
 
-detaly.addEventListener('click', detalyFn)
-function detalyFn(e) {
-    e.preventDefault();
-    detalisation = document.querySelector('.detalisation');
-    wrapperLeft = document.querySelector('.wrapper_left');
-    wrapperRigth = document.querySelector('.wrapper_right');
-    detalisation.style.display = 'flex';
-    wrapperLeft.style.display = 'none';
-    wrapperRigth.style.display = 'none';
 
-}*/
 
 /*
 function foo() {
@@ -129,7 +117,19 @@ function odomFn(elem) {
 
 }
 
+const detaly = document.querySelector('.detaly');
 
+detaly.addEventListener('click', detalyFn)
+function detalyFn(e) {
+    e.preventDefault();
+    detalisation = document.querySelector('.detalisation');
+    wrapperLeft = document.querySelector('.wrapper_left');
+    wrapperRigth = document.querySelector('.wrapper_right');
+    detalisation.style.display = 'flex';
+    wrapperLeft.style.display = 'none';
+    wrapperRigth.style.display = 'none';
+
+}
 
 
 
