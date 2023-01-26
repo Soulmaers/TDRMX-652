1fd37180ea3c3cc7ca22da9241e78c0b6f45e55a
Soulmaers <certiss@ya.ru>
2023-01-26 11:12:39 +0300

    изменено приложение для tdrmx

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const path = require('path')
 
-const routes = require('./settings/routes')
-const routes2 = require('./settings/routes')
+
 const app = express();
 
 
@@ -13,19 +12,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
-require('./middleware/passport.js')(passport);
+
 
 const indexHTML = path.resolve(__dirname, './public/index.html');
 app.use('/', express.static('public'));
 app.get('/', (req, res) => res.sendFile(indexHTML));
 
 
+module.exports = app;
 
 
-routes(app);
-
-
-module.exports = app
 
 
 
